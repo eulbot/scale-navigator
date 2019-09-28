df9e3056a39b8bb6e97867d53370e62e3d02cbe7
Fischer, Ulrich <ulrich.fischer@hexagongeospatial.com>
2019-09-28 22:08:23 +0200

    Replace segments array with map

--- lib/scale-navigator/component.tsx
+++ lib/scale-navigator/component.tsx
@@ -5,11 +5,21 @@ interface IProps {
     map?: any
 }
 
+interface IExtent {
+    from: number; 
+    to: number; 
+    range: number;
+}
+
+interface ISegment {
+    position: number;
+    value: number;
+}
+
 interface IState {
     factor: number;
-    offset: number;
-    segments: Array<{ position: number; value: number }>;
-    segmentWidth: number;
+    extent: IExtent;
+    segments: Map<number, number>;
 }
 
 export class ScaleNavigator extends React.Component<IProps, IState> {
@@ -21,9 +31,8 @@ export class ScaleNavigator extends React.Component<IProps, IState> {
 
         this.state = {
             factor: 1,
-            offset: 0,
-            segmentWidth: 0,
-            segments: []
+            extent: { from: -100, to: 100, get range() { return this.to - this. from } },
+            segments: new Map()
         }
 
         this.target = React.createRef<HTMLDivElement>();
@@ -32,9 +41,15 @@ export class ScaleNavigator extends React.Component<IProps, IState> {
 
     scroll = (e: React.WheelEvent<HTMLDivElement>) => {
 
+        let extent = this.state.extent;
+        extent.to += e.deltaY / 10;
+        extent.from -= e.deltaY / 10;
+
         this.setState({
-            factor: Math.max(1, this.state.factor + (e.deltaY * -1))
-        })
+            extent: extent
+        });
+        
+        this.initSegments();
     }
 
     componentDidMount() {
@@ -46,35 +61,36 @@ export class ScaleNavigator extends React.Component<IProps, IState> {
         if (!this.target.current)
             return;
 
-        let from = 30;
-        let to = 875;
-
-        let range = to - from;
+        let extent = this.state.extent;
         let width = this.target.current.clientWidth;
-        let factor = width / range;
+        this.ruler(this.state.segments, extent, width);
+    }
 
-        let power = Math.pow(10, Math.floor(Math.log10(range)));
-        let segments = [];
-        let offset = from % power;
+    ruler = (segments: Map<number, number>, extent: IExtent, width: number) => {
 
-        for (let i = from - offset; i < to + power; i += power) {
+        let usedKeys = [];
+        let slope = (x: number) => x * (width / extent.range) + (width / 2);
+        let power = Math.pow(10, Math.floor(Math.log10(extent.range)));
+        let offset = extent.from % power + power;
 
-            segments.push({ value: i, position: (i - offset) * factor });
+        for (let i = extent.from - offset; i < extent.to + power; i += power) {
+            segments.set(i, slope(i));
+            usedKeys.push(i);
         }
 
-        this.setState({
-            segments: segments
-        });
+        // Drop unused entries
+        [...segments.keys()].filter(s => !usedKeys.includes(s)).map(d => segments.delete(d));
     }
 
     render() {
 
         return (
             <div className="scale-navigator-container" onWheel={(e) => this.scroll(e)} ref={this.target}>
-                {this.state.segments.map((segment, index) => (
-
-                    <RulerSegment key={index} value={segment.value} position={segment.position} />
-                ))}
+                {
+                    [...this.state.segments].map(([value, position]) => {
+                        return <RulerSegment key={value} value={value} position={position} />
+                    })
+                }
             </div>
         )
     }
